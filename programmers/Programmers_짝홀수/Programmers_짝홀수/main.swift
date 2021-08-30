d6f939acb944f358abad538bf8993bb8c098f0cd
LIMJISEONG <yim2627@gmail.com>
2021-08-30 21:14:05 +0900

    Add Programmers OddEven
    Easy!!

---
+++ programmers/Programmers_짝홀수/Programmers_짝홀수/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Programmers_짝홀수
+//
+//  Created by 임지성 on 2021/08/30.
+//
+
+import Foundation
+
+func solution(_ num:Int) -> String {
+    return num % 2 == 0 ? "Even" : "Odd"
+}
+
+print(solution(4))
+
+
