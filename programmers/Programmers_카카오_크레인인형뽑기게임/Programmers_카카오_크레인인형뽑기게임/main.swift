5d738d5630c6231eab4a36099590c3d5954fc7d6
LIMJISEONG <yim2627@gmail.com>
2021-09-12 19:07:12 +0900

    Add Programmers
    Add Programmers 카카오 크레인인형뽑기게임

---
+++ programmers/Programmers_카카오_크레인인형뽑기게임/Programmers_카카오_크레인인형뽑기게임/main.swift
@@ -0,0 +1,51 @@
+//
+//  main.swift
+//  Programmers_카카오_크레인인형뽑기게임
+//
+//  Created by 임지성 on 2021/09/12.
+//
+
+import Foundation
+
+func solution(_ board: [[Int]], _ moves: [Int]) -> Int {
+    
+    var copiedboard = board
+    var bucket = [Int]()
+    var removeCnt = 0
+    
+    moves.forEach { move in
+        let trulyMoves = move - 1
+        for idx in 0..<copiedboard.count {
+            let doll = copiedboard[idx][trulyMoves]
+            guard doll != 0 else {
+                continue
+            }
+            copiedboard[idx][trulyMoves] = 0
+            if bucket.last == doll {
+                bucket.removeLast()
+                removeCnt += 2
+            } else {
+                bucket.append(doll)
+            }
+            break
+        }
+    }
+    
+    return removeCnt
+}
+
+print(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]))
+
+/*
+ [
+ [0,0,0,0,0],
+ [0,0,1,0,3],
+ [0,2,5,0,1],
+ [4,2,4,4,2],
+ [3,5,1,3,1]
+ ]
+ 
+ 4
+ 2
+ 4
+ */
