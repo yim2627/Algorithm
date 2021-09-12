dc46106ff3ddbbf664e1683527c0fc292afe95a8
LIMJISEONG <yim2627@gmail.com>
2021-09-12 19:07:57 +0900

    Fix Progammers 인형뽑기게임

--- programmers/Programmers_카카오_크레인인형뽑기게임/Programmers_카카오_크레인인형뽑기게임/main.swift
+++ programmers/Programmers_카카오_크레인인형뽑기게임/Programmers_카카오_크레인인형뽑기게임/main.swift
@@ -34,7 +34,6 @@ func solution(_ board: [[Int]], _ moves: [Int]) -> Int {
     return removeCnt
 }
 
-print(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]))
 
 /*
  [
