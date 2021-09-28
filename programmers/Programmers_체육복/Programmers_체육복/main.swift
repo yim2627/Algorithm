0875b77452f91c12356c55238adcb1cd0cad3161
LIMJISEONG <yim2627@gmail.com>
2021-09-28 16:14:56 +0900

    Update Programmers 체육복
    Add Comment

--- programmers/Programmers_체육복/Programmers_체육복/main.swift
+++ programmers/Programmers_체육복/Programmers_체육복/main.swift
@@ -10,18 +10,18 @@ import Foundation
 func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
     var answer = 0
     var lostSet = Set(lost).subtracting(reserve)
-    let reserveSet = Set(reserve).subtracting(lost)
+    let reserveSet = Set(reserve).subtracting(lost) // 중복제거(Set), Sutract(차집합) 이유 - 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. (제한사항 5)
     
     for reserve in reserveSet {
-        if lostSet.contains(reserve - 1) { //
-            lostSet.remove(reserve - 1)
+        if lostSet.contains(reserve - 1) { // 앞사람 먼저 확인 
+            lostSet.remove(reserve - 1) // 체육복 없는 사람(lost)에게 빌려줬기떄문에 lost에서 빠지게 됨
             continue
         }
-        if lostSet.contains(reserve + 1) {
-            lostSet.remove(reserve + 1)
+        if lostSet.contains(reserve + 1) { // 뒷사람 확인
+            lostSet.remove(reserve + 1) // 체육복 없는 사람(lost)에게 빌려줬기떄문에 lost에서 빠지게 됨
         }
     }
-    answer = n - lostSet.count
+    answer = n - lostSet.count // 전체 인원 - 체욱복 없는 사람수
     
     return answer
 }
