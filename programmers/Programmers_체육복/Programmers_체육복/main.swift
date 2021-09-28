976d84607233787771d9c770dee5dae1e016cf78
LIMJISEONG <yim2627@gmail.com>
2021-09-28 16:15:16 +0900

    Update Programmers 체육복
    Update Comment

--- programmers/Programmers_체육복/Programmers_체육복/main.swift
+++ programmers/Programmers_체육복/Programmers_체육복/main.swift
@@ -13,7 +13,7 @@ func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
     let reserveSet = Set(reserve).subtracting(lost) // 중복제거(Set), Sutract(차집합) 이유 - 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. (제한사항 5)
     
     for reserve in reserveSet {
-        if lostSet.contains(reserve - 1) { // 앞사람 먼저 확인 
+        if lostSet.contains(reserve - 1) { // 앞사람 먼저 확인 (Greedy)
             lostSet.remove(reserve - 1) // 체육복 없는 사람(lost)에게 빌려줬기떄문에 lost에서 빠지게 됨
             continue
         }
